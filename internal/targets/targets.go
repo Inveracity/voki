452c2fb213166f1e08dece8234d4c3ef9708ef44
Inveracity <cbaklid@gmail.com>
2025-03-14 07:50:14 +0100

    move target struct to its own file

--- internal/targets/targets.go
+++ internal/targets/targets.go
@@ -10,37 +10,6 @@ import (
 	"github.com/zclconf/go-cty/cty/function"
 )
 
-type Configuration struct {
-	Targets []Target `hcl:"target,block"`
-	Tasks   []Task   `hcl:"task,block"`
-	Steps   []Step   `hcl:"step,block"`
-}
-
-type Target struct {
-	Name  string  `hcl:"name,label"`
-	User  *string `hcl:"user"`
-	Host  string  `hcl:"host"`
-	Steps []Step  `hcl:"step,block"`
-}
-
-type Step struct {
-	Action      string   `hcl:"action,label"`
-	Command     string   `hcl:"command,optional"`
-	Task        string   `hcl:"task,optional"`
-	Use         []string `hcl:"use,optional"`
-	Source      string   `hcl:"source,optional"`
-	Destination string   `hcl:"destination,optional"`
-	Mode        string   `hcl:"mode,optional"`
-	Sudo        bool     `hcl:"sudo,optional"`
-	Shell       string   `hcl:"shell,optional"`
-	Chown       string   `hcl:"chown,optional"`
-}
-
-type Task struct {
-	Name  string `hcl:"task,label"`
-	Steps []Step `hcl:"step,block"`
-}
-
 // ParseHCL takes a byte array, from os.ReadFile
 func ParseHCL(in []byte) (*Configuration, error) {
 	parser := hclparse.NewParser()
